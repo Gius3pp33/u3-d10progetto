151c6e08b280ea1e98bb65236f4f5907b1dbab13
Gius3pp33 <peppe-2@live.it>
2024-07-06 13:16:54 +0200

    migliorato il campo input per selzionare la città

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,36 +1,97 @@
-import React, { useState } from 'react';
-import { Button, Container, Form } from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
+import { Button, Container, Form, ListGroup, Alert } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
+
+const API_KEY = '1e3b33eb0c23931c8d2c3a6ad26d0a97'; 
 
 function SearchBar({ setCity }) {
   const [input, setInput] = useState('');
+  const [suggestions, setSuggestions] = useState([]);
+  const [error, setError] = useState('');
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchSuggestions = async () => {
+      if (input) {
+        try {
+          const response = await fetch(
+            `http://api.openweathermap.org/data/2.5/find?q=${input}&type=like&sort=population&cnt=5&appid=${API_KEY}`
+          );
+          const data = await response.json();
+          if (data.list) {
+            setSuggestions(data.list.map(city => city.name));
+            setError('');
+          } else {
+            setSuggestions([]);
+           
+          }
+        } catch (error) {
+          console.error('Errore durante il fetching dei suggerimenti:', error);
+          setSuggestions([]);
+          setError('Errore durante il fetching dei suggerimenti');
+        }
+      } else {
+        setSuggestions([]);
+        setError('');
+      }
+    };
+
+    fetchSuggestions();
+  }, [input]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setCity(input);
+    if (suggestions.includes(input)) {
+      setCity(input);
+      navigate('/weather');
+      setInput('');
+      setSuggestions([]);
+      setError('');
+    } else {
+      setError('Città non esistente.Digita una città esistente o seleziona una città dalla lista dei suggerimenti.');
+    }
+  };
+
+  const handleSuggestionClick = (suggestion) => {
+    setCity(suggestion);
     navigate('/weather');
     setInput('');
+    setSuggestions([]);
+    setError('');
   };
 
   return (
-    <Container fluid >
+    <Container fluid>
       <h1 id='title-page'>EpiMeteo</h1>
       <Container>
-      <Form onSubmit={handleSubmit} className="d-flex  align-items-center justify-content-center mb-4 ">
-        <Form.Group >
-          <Form.Control
-            type="text"
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-            placeholder="Cerca una città..."
-            required
-          />
-        </Form.Group>
-        <Button id='btn-title' type="submit" >
-          Cerca
-        </Button>
-      </Form>
+        <Form onSubmit={handleSubmit} className="d-flex align-items-center justify-content-center mb-4">
+          <Form.Group className="position-relative">
+            <Form.Control
+              type="text"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              placeholder="Cerca una città..."
+              required
+            />
+            {suggestions.length > 0 && (
+              <ListGroup className="position-absolute w-100">
+                {suggestions.map((suggestion, index) => (
+                  <ListGroup.Item 
+                    key={index} 
+                    action 
+                    onClick={() => handleSuggestionClick(suggestion)}
+                  >
+                    {suggestion}
+                  </ListGroup.Item>
+                ))}
+              </ListGroup>
+            )}
+          </Form.Group>
+          <Button id='btn-title' type="submit">
+            Cerca
+          </Button>
+        </Form>
+        {error && <Alert variant="danger" className="mt-3">{error}</Alert>}
       </Container>
     </Container>
   );
